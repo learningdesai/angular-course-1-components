7708e8b2433f64fac2d3f5647bb3e3b495715142
learningdesai <learningdesai@gmail.com>
2023-03-20 17:36:11 +0530

    Pipes in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,13 @@ import { Course } from './model/course';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-courses=COURSES;
 
+  courses=COURSES;
+
+  startDate= new Date(2000,0,1); 
+  price:number=9.99
+  title=COURSES[2].description;
+course=COURSES[2];
 onCourseClick(){
   console.log("App Component ..Click Event");
 }
